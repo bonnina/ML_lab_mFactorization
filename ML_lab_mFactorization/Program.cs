ac732ef8b5336a678765e963c2f3b3d63672e08a
bonnina <37539513+bonnina@users.noreply.github.com>
2020-03-31 23:27:12 +0300

    Add BuildAndTrainModel method

--- ML_lab_mFactorization/Program.cs
+++ ML_lab_mFactorization/Program.cs
@@ -12,6 +12,8 @@ namespace ML_lab_mFactorization
             MLContext mlContext = new MLContext();
 
             (IDataView trainingDataView, IDataView testDataView) = LoadData(mlContext);
+
+            ITransformer model = BuildAndTrainModel(mlContext, trainingDataView);
         }
 
         public static (IDataView training, IDataView test) LoadData(MLContext mlContext)
@@ -23,6 +25,28 @@ namespace ML_lab_mFactorization
             IDataView testDataView = mlContext.Data.LoadFromTextFile<MovieRating>(testDataPath, hasHeader: true, separatorChar: ',');
 
             return (trainingDataView, testDataView);
+        }
+
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainingDataView)
+        {
+            IEstimator<ITransformer> estimator = mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "userIdEncoded", inputColumnName: "userId")
+            .Append(mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "movieIdEncoded", inputColumnName: "movieId"));
+
+            var options = new MatrixFactorizationTrainer.Options
+            {
+                MatrixColumnIndexColumnName = "userIdEncoded",
+                MatrixRowIndexColumnName = "movieIdEncoded",
+                LabelColumnName = "Label",
+                NumberOfIterations = 20,
+                ApproximationRank = 100
+            };
+
+            var trainerEstimator = estimator.Append(mlContext.Recommendation().Trainers.MatrixFactorization(options));
+
+            Console.WriteLine("=============== Training the model ===============");
+            ITransformer model = trainerEstimator.Fit(trainingDataView);
+
+            return model;
         }
     }
 }
